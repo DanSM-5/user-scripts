c1d883feb979283e9f3464187604f3d623ccb4cc
Eduardo Sanchez <eduardo.sanchez@accedo.tv>
2022-10-19 14:18:03 -0400

    update: tizen scripts

--- tizen/th.ps1
+++ tizen/th.ps1
@@ -1,6 +1,12 @@
 # PS1 wrapper for pwsh
 
+# Fix path variables before passing them to bash
+$args_array = 0..$args.count
+for ( $i = 0; $i -lt $args.count; $i++ ) {
+  $args_array[$i] = "$($args[$i] -replace '\\', '/')"
+}
+
 # Find gitbash and no a wsl bash
 $__gitbash__ = $(where.exe bash | grep 'Git\\usr\\bin\\bash')
 
-& $__gitbash__ --norc -ilc "tizen-help $args"
+& $__gitbash__ --norc -ilc "tizen-help $args_array"
