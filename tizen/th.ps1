2f44245ea3fe67d085c9e178b5fc19b918634d3e
Eduardo Sanchez <eduardo.sanchez@accedo.tv>
2022-11-01 18:24:51 -0400

    feat: Improve script and pwsh wrapper

--- tizen/th.ps1
+++ tizen/th.ps1
@@ -9,4 +9,10 @@ for ( $i = 0; $i -lt $args.count; $i++ ) {
 # Find gitbash and no a wsl bash
 $__gitbash__ = $(where.exe bash | grep 'Git\\usr\\bin\\bash')
 
-& $__gitbash__ --norc -ilc "tizen-help $args_array"
+$CHROMIUM = if ($env:CHROMIUM) { $env:CHROMIUM -replace '\\', '/' } else { $($HOME -replace '\\', '/') + '/AppData/Local/Chromium/Application/chrome.exe' }
+$CHROMIUM = if ($CHROMIUM -match ':') { "/c" + $CHROMIUM.substring(2) } else { $CHROMIUM }
+$IP = $env:SAMSUNG_DEVICE_IP
+$WEB_SECURITY = $env:DISABLE_WEB_SECURITY
+
+& $__gitbash__ --norc -ilc "CHROMIUM='$CHROMIUM' SAMSUNG_DEVICE_IP='$IP' DISABLE_WEB_SECURITY='$WEB_SECURITY' tizen-help $args_array"
+
