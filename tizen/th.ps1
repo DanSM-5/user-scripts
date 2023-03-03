6c1bad6d52a4107e8f8d8d5017641fb4c4d7a266
Eduardo Sanchez <eduardo.sanchez@accedo.tv>
2023-03-03 10:42:31 -0500

    feat: Update tizen-help command

--- tizen/th.ps1
+++ tizen/th.ps1
@@ -1,16 +1,29 @@
 # PS1 wrapper for pwsh
 
+# function parse-path ([string] $original) {
+#   # Change \ for / in paths.
+#   $parsed = "$($original -replace '\\', '/')"
+
+#   # Update absolute paths with posix paths
+#   if ($parsed -match ':') {
+#     return '/' + $parsed.substring(0, 1).toLower() + $parsed.substring(2)
+#   } else {
+#     return $parsed
+#   }
+# }
+
 # Fix path variables before passing them to bash
 $args_array = 1..$args.count
 for ( $i = 0; $i -lt $args.count; $i++ ) {
-  $args_array[$i] = "$($args[$i] -replace '\\', '/')"
+  $parsed = "$($args[$i] -replace '\\', '/')"
+  $args_array[$i] = if ($parsed -match ':') { '/' + $parsed.substring(0, 1).toLower() + $parsed.substring(2) } else { $parsed }
 }
 
 # Find gitbash and no a wsl bash
 $__gitbash__ = $(where.exe bash | grep 'Git\\usr\\bin\\bash')
 
 $CHROMIUM = if ($env:CHROMIUM) { $env:CHROMIUM -replace '\\', '/' } else { $($HOME -replace '\\', '/') + '/AppData/Local/Chromium/Application/chrome.exe' }
-$CHROMIUM = if ($CHROMIUM -match ':') { "/c" + $CHROMIUM.substring(2) } else { $CHROMIUM }
+$CHROMIUM = if ($CHROMIUM -match ':') { '/' + $CHROMIUM.substring(0, 1).toLower() + $CHROMIUM.substring(2) } else { $CHROMIUM }
 $IP = $env:SAMSUNG_DEVICE_IP
 $WEB_SECURITY = $env:DISABLE_WEB_SECURITY
 
