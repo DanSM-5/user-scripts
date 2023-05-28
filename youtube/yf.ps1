1f693f7b0ae3ec980363f48e7af6acca0019a0ac
Eduardo Sanchez <eduardo.sanchez@accedo.tv>
2023-05-28 00:43:35 -0400

    fix: Update yf and yfd powershell wrappers

--- youtube/yf.ps1
+++ youtube/yf.ps1
@@ -4,10 +4,31 @@
 Push-Location "$PSScriptRoot"
 
 # Find gitbash and no a wsl bash
-$__gitbash__ = $(where.exe bash | grep 'Git\\usr\\bin\\bash')
+# $__gitbash__ = $(where.exe bash | grep 'Git\\usr\\bin\\bash')
+
+# Find env in git installation directly
+$__gitenv__ = $(where.exe env | grep 'Git\\usr\\bin\\env')
 
 # Run yf command
-& $__gitbash__ --norc -ilc "ytfzf-wrapper.sh yf $args"
+# & $__gitbash__ --norc -ilc "ytfzf-wrapper.sh yf $args"
+
+# Environment variables to setup bash from git for windows
+$GITBASH_ENVIRONMENT = @(
+  # Enable MINGW work as running gitbash directly
+  "MSYS='enable_pcon'"
+  "MSYSTEM='MING64'"
+  "enable_pcon='1'"
+  # Avoid POSIX to WINDOWS path conversions
+  # "MSYS_NO_PATHCONV='1'"
+  # "MSYS2_ARG_CONV_EXCL='*'"
+  # Add ENV variables for tizen-help
+  "CHROMIUM='$CHROMIUM'"
+  "SAMSUNG_DEVICE_IP='$IP'"
+  "DISABLE_WEB_SECURITY='$DISABLE_WEB_SECURITY'"
+)
+
+# Required to add /usr/bin in the path for the hashbang in tizen-help script to work
+& $__gitenv__ $GITBASH_ENVIRONMENT /usr/bin/bash -c "export PATH=`"/usr/bin:/usr/local/bin:`$PATH`"; ytfzf-wrapper.sh yf $args"
 
 Pop-Location
 
