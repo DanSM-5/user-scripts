236ec17918f9da5857b38ab23a433f1c3c49512d
daniel <eduardo.daniel.s.m@gmail.com>
2023-11-14 01:42:19 -0500

    fix: Argument parsing in lf.ps1 wrapper

--- bin/lf.ps1
+++ bin/lf.ps1
@@ -2,13 +2,14 @@
 
 # Important to use @args and no $args when sending arguments to this script
 
+$__gitenv__ = $(where.exe env | grep 'Git\\usr\\bin\\env')
+$__gitbash__ = $(where.exe bash | grep 'Git\\usr\\bin\\bash')
+
 $__apend_path__ = "export PATH=`"/usr/bin:`$PATH`";"
 $APPENDED_ENVIRONMENT = @(
   "PATH=`"/usr/bin:`$PATH`""
-  "NEW=test"
 )
 
-$__gitenv__ = $(where.exe env | grep 'Git\\usr\\bin\\env')
 $GITBASH_ENVIRONMENT = @(
   # Enable MINGW work as running gitbash directly
   "MSYS='enable_pcon'"
@@ -20,5 +21,19 @@ $GITBASH_ENVIRONMENT = @(
   # "MSYS2_ARG_CONV_EXCL='*'"
 )
 
-& "$__gitenv__" $GITBASH_ENVIRONMENT /usr/bin/bash -c "$APPENDED_ENVIRONMENT lf.exe $args"
+$COMMAND_ARGS = 1..$args.length
+
+for ( $i = 0; $i -lt $args.length; $i++ ) {
+  $arg_value = $args[$i]
+
+  # Use gitbash printf to escape all arguments to lf in bash context
+  $arg_value = "$(& "$__gitbash__" -c "printf '%q' '$arg_value'")"
+  # Then lets wrap all arguments in single quotes
+  $COMMAND_ARGS[$i] = "'$arg_value'"
+}
+
+# Call a prepared shell command. It adds MINGW64 environment variables,
+# adds /usr/bin to the start of the path and forward all escaped command arguments 
+& "$__gitenv__" $GITBASH_ENVIRONMENT /usr/bin/bash -c "$APPENDED_ENVIRONMENT lf.exe $COMMAND_ARGS"
+# Write-Host "&" "$__gitenv__" "$GITBASH_ENVIRONMENT" /usr/bin/bash -c "$APPENDED_ENVIRONMENT lf.exe $COMMAND_ARGS"
 
