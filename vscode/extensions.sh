99b3c05aa8c224eece70e052b008099009d9282b
daniel <eduardo.daniel.s.m@gmail.com>
2024-03-06 21:36:31 -0500

    feat: Add extensions for vscode

---
+++ vscode/extensions.sh
@@ -0,0 +1,17 @@
+asvetliakov.vscode-neovim
+colder.source-map-visualization
+dbaeumer.vscode-eslint
+# carbonid1.emmet-jss
+donjayamanne.githistory
+# dsznajder.es7-react-js-snippets
+# firefox-devtools.vscode-firefox-debug
+jasonnutter.search-node-modules
+# ms-python.debugpy
+# ms-python.python
+# ms-python.vscode-pylance
+# ms-vscode-remote.remote-wsl
+ritwickdey.liveserver
+# streetsidesoftware.code-spell-checker
+sumneko.lua
+vscode-icons-team.vscode-icons
+
