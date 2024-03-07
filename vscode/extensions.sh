bd475153e65717cc3920f77651b5bbac3435f8cd
Eduardo Sanchez <eduardo.sanchez@accedo.tv>
2024-03-07 10:42:11 -0500

    refactor: Update vscode extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,17 +1,32 @@
 asvetliakov.vscode-neovim
-colder.source-map-visualization
-dbaeumer.vscode-eslint
 # carbonid1.emmet-jss
+colder.source-map-visualization
+cssho.vscode-svgviewer
+dbaeumer.vscode-eslint
 donjayamanne.githistory
+# dotjoshjohnson.xml
 # dsznajder.es7-react-js-snippets
-# firefox-devtools.vscode-firefox-debug
+esbenp.prettier-vscode
+firefox-devtools.vscode-firefox-debug
+# github.vscode-github-actions
+# hex-ci.stylelint-plus
 jasonnutter.search-node-modules
+# mads-hartmann.bash-ide-vscode
+# mathiasfrohlich.kotlin
+# monasticpanic.vscode-astroturf
+# ms-dotnettools.csharp
+# ms-dotnettools.vscode-dotnet-runtime
 # ms-python.debugpy
+# ms-python.isort
 # ms-python.python
 # ms-python.vscode-pylance
 # ms-vscode-remote.remote-wsl
+peakchen90.open-html-in-browser
 ritwickdey.liveserver
+# shd101wyy.markdown-preview-enhanced
 # streetsidesoftware.code-spell-checker
-sumneko.lua
+# sumneko.lua
+# syler.sass-indented
+thejustinwalsh.textproto-grammer
+vs-publisher-1448185.keyoti-changeallendoflinesequence
 vscode-icons-team.vscode-icons
-
