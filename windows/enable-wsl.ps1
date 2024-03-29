f814e00a6c1033ba77a3d5b2784a364081c8769d
daniel <daniel@daniel-predator.env>
2024-03-29 17:35:37 -0400

    feat: Add windows scripts for setup

---
+++ windows/enable-wsl.ps1
@@ -0,0 +1,5 @@
+#! /usr/bin/env pwsh
+
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
---
+++ windows/group-policy-install.bat
@@ -0,0 +1,7 @@
+@echo off
+
+FOR %F IN ("%SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~*.mum") DO (DISM /Online /NoRestart /Add-Package:"%F")
+FOR %F IN ("%SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~*.mum") DO (DISM /Online /NoRestart /Add-Package:"%F")
+
+REM pause
+
