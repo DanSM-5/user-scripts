6e0986e507d5b5769565ed678ccb181ca789fc57
daniel <eduardo.daniel.s.m@gmail.com>
2024-04-19 22:26:43 -0400

    feat(With-Env): Add script to mimic posix prepending environment variable assignment

---
+++ powershell/With-Env.ps1
@@ -0,0 +1,46 @@
+
+$ori = @{}
+Try {
+  $i = 0
+
+  # Loading .env files
+  if(Test-Path $args[0]) {
+    foreach($line in (Get-Content $args[0])) {
+      if($line -Match '^\s*$' -Or $line -Match '^#') {
+        continue
+      }
+
+      $key, $val = $line.Split("=")
+      $ori[$key] = if(Test-Path Env:\$key) { (Get-Item Env:\$key).Value } else { "" }
+      New-Item -Name $key -Value $val -ItemType Variable -Path Env: -Force > $null
+    }
+
+    $i++
+  }
+
+  while(1) {
+    if($i -ge $args.length) {
+      exit
+    }
+
+    # Stop look  on first argument without '=' sign
+    # if(!($args[$i] -Match '^[^ ]+=[^ ]+$')) {
+    if(!($args[$i] -Match '.+=.*')) {
+      break
+    }
+
+    $key, $val = $args[$i].Split("=")
+    # $val = if ($val) { $val } else { "" }
+    $ori[$key] = if(Test-Path Env:\$key) { (Get-Item Env:\$key).Value } else { "" }
+    New-Item -Name $key -Value $val -ItemType Variable -Path Env: -Force > $null
+
+    $i++
+  }
+
+
+  Invoke-Expression ($args[$i..$args.length] -Join " ")
+} Finally {
+  foreach($key in $ori.Keys) {
+    New-Item -Name $key -Value $ori.Item($key) -ItemType Variable -Path Env: -Force > $null
+  }
+}
