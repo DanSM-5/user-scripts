2b31694988765b1bf6e8358251595758a79887f1
Eduardo Sanchez <eduardo.sanchez@accedo.tv>
2024-04-21 16:44:05 -0400

    feat(lfcd): Fix lfcd command on other platforms

--- bin/lfcd.ps1
+++ bin/lfcd.ps1
@@ -1,3 +1,4 @@
+#!/usr/bin/env pwsh
 
 # Change working dir in powershell to last dir in lf on exit.
 #
@@ -16,5 +17,10 @@
 
 if (Get-Command -Name "lf.exe" -ErrorAction SilentlyContinue) {
   $(lf.ps1 -print-last-dir @args) | Set-Location
+}
+
+# For user in other platforms
+if ((Get-Command -Name "lf" -ErrorAction SilentlyContinue) -and !$IsWindows) {
+  lf -print-last-dir @args | Set-Location
 }
 
