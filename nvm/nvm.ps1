c3aa94605cc7cceed7fd5d67587699dabd57a838
daniel <eduardo.daniel.s.m@gmail.com>
2024-04-30 14:12:34 -0400

    feat(nvm): Add support for nvm install for NVM4W

--- nvm/nvm.ps1
+++ nvm/nvm.ps1
@@ -3,9 +3,12 @@
 if ($IsWindows) {
   # Check if there the only argument is use
   # Otherwise, forward arguments to real nvm
-  if (($args[0] -eq 'use') -and ($args[1] -eq $null)) {
+  if (($args[0] -eq 'use') -and ($args[1] -eq $null) -and (Test-Path "$PSD/.nvmrc" -ea 0)) {
     $nvm_version = Get-Content "$PWD/.nvmrc"
     & "$env:NVM_HOME\nvm.exe" use $nvm_version
+  } elseif (($args[0] -eq 'install') -and ($args[1] -eq $null) -and (Test-Path "$PSD/.nvmrc" -ea 0)) {
+    $nvm_version = Get-Content "$PWD/.nvmrc"
+    & "$env:NVM_HOME\nvm.exe" install $nvm_version
   } else {
     & "$env:NVM_HOME\nvm.exe" $args
   }
