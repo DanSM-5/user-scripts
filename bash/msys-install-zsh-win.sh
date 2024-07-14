531071ee9c08929d65303b2e85b326d023f1708d
daniel <eduardo.daniel.s.m@gmail.com>
2024-07-13 22:18:32 -0400

    feat: Add script to install zsh in gitbash

---
+++ bash/msys-install-zsh-win.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+if ! command -v zstd &>/dev/null; then
+  printf "%s\n" "Missing zstd package to extract '.tar.zst'"
+  printf "%s\n" "Hint: scoop install zstd"
+  exit
+fi
+
+if ! command -v gsudo &>/dev/null; then
+  printf "%s\n" "Missing 'gsudo' package to elevate copy process"
+  printf "%s\n" "Hint: scoop install gsudo"
+  exit
+fi
+
+if ! command -v rclone &>/dev/null; then
+  printf "%s\n" "Missing 'rclone' package to copy safely"
+  printf "%s\n" "Hint: scoop install rclone"
+  exit
+fi
+
+temp_zsh_dir="$TEMP/zsh"
+tarball_url="https://mirror.msys2.org/msys/x86_64/zsh-5.9-2-x86_64.pkg.tar.zst"
+gitbash_dir="$(where.exe bash | grep 'Git\\usr\\bin\\bash' | sed 's/\\usr\\bin\\bash.exe//')"
+# Generated with wcurl. Requires updated curl to work
+curl_ops=(
+  --globoff
+  --location
+  --no-clobber
+  --proto-default
+  https
+  --remote-name-all
+  --remote-time
+  --retry
+  10
+  --retry-max-time
+  10
+)
+
+cleanup () {
+  if test -d "$temp_zsh_dir"; then
+    rm -rf -- "$temp_zsh_dir"
+  fi
+}
+
+# Remove tmp dir on exit
+trap cleanup EXIT
+
+# Yes, another zsh nested directory
+mkdir -p "$temp_zsh_dir/zsh"
+pushd "$temp_zsh_dir"
+# Download zsh
+curl "${curl_ops[@]}" "$tarball_url"
+# Extract zsh archive. Name could vary but the end should remain the same, so use glob
+tar --zstd -xvf *x86_64.pkg.tar.zst --directory "$temp_zsh_dir/zsh"
+# Copy files. Using gsudo to elevate and rclone to copy only new files (and because I don't know better... sorry).
+gsudo rclone -v -u ./zsh "$gitbash_dir"
+popd
+
+printf "%s\n" "Testing if it works..."
+printf "%s\n" "$(which zsh)"
+
