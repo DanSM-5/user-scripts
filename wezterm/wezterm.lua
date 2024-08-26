5e42c1020345fea1dca18ff73667d45466448a58
daniel <daniel@daniel-predator.env>
2024-08-26 19:50:23 -0400

    feat: Configure keybindings in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 -- Wezterm API
 local wezterm = require('wezterm')
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -45,9 +46,48 @@ config.window_background_opacity = 0.85
 --   }
 -- }
 
+-- Enable if using environment variable instead of command line arguments
 -- if os.getenv('WEZTERM_QUAKE_MODE') == '1' then
 --   config.window_decorations = 'NONE'
 -- end
+
+if wezterm.gui then
+  local insert_key = function (mode, key_config)
+    table.insert(
+      mode,
+      key_config
+    )
+  end
+
+  -- copy mode overrides
+  local copy_mode = wezterm.gui.default_key_tables().copy_mode
+  local copy_mode_overrides = {
+    -- sane default arrow key word navigation
+    { key = 'RightArrow', mods = 'CTRL', action = act.CopyMode('MoveForwardWord') },
+    { key = 'LeftArrow', mods = 'CTRL', action = act.CopyMode('MoveBackwardWord') }
+  }
+  for _, key in ipairs(copy_mode_overrides) do
+    insert_key(copy_mode, key)
+  end
+
+  -- add overrides
+  config.key_tables = {
+    copy_mode = copy_mode
+  }
+
+  -- update key events (no mode)
+  config.keys = {
+    { key = 'M', mods = 'CTRL|SHIFT', action = act.ActivateCopyMode },
+    { key = 'L', mods = 'ALT|SHIFT', action = act.ClearScrollback('ScrollbackOnly') },
+    { key = 'K', mods = 'CTRL|SHIFT', action = act.ScrollByPage(-1) },
+    { key = 'J', mods = 'CTRL|SHIFT', action = act.ScrollByPage(1) },
+  }
+end
+
+if os.getenv('IS_STEAMDECK') == 'true' then
+  -- Enable scroll bar for scrolling with pointer only like steam deck
+  config.enable_scroll_bar = true
+end
 
 return config
 
