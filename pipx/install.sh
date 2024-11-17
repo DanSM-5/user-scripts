2c5c55c8c5410730d2d8ef0eef03e3bd7bc16caa
daniel <daniel@daniel-predator.env>
2024-11-17 12:16:39 -0500

    feat: Add script for common installed programs with pipx

---
+++ pipx/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Common packages installed with pipx
+
+pipx install ani
+pipx install animdl
+pipx install yt-dlp
+pipx install gallery-dl &&
+  pipx inject gallery-dl yt-dlp
+pipx install neovim-remote
+pipx install speedtest-cli
+pipx install vpk
+
+pipx install mov-cli &&
+  pipx inject  mov-cli beautifulsoup4 &&
+  pipx inject  mov-cli yt-dlp &&
+  pipx inject  mov-cli mov-cli-youtube &&
+  pipx inject  mov-cli otaku-watcher &&
+  pipx inject  mov-cli film-central &&
+  pipx inject  mov-cli mov-cli-files
