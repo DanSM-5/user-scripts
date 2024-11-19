a903dfea053fba9a80b276cb7e36cd65a03e907a
daniel <eduardo.daniel.s.m@gmail.com>
2024-11-19 18:16:59 -0500

    feat(git-search): Add powershell version of script

---
+++ bin/git-search-commits.ps1
@@ -0,0 +1,109 @@
+#!/usr/bin/env pwsh
+
+[CmdletBinding()]
+Param(
+  # Mode log
+  [Switch] $Log = $false,
+  # Mode regex
+  [Switch] $Regex = $false,
+  # Mode string
+  [Switch] $String = $false,
+  # Manual mode
+  [ValidateSet('log', 'regex', 'string')]
+  [Switch] $Mode = '',
+  # Edit
+  [Switch] $Edit = $false,
+  [Switch] $Help = $false,
+  [Parameter(ValueFromRemainingArguments = $true)]
+  [String] $Query = ''
+)
+
+$cmd_mode = $Mode
+
+# Set mode
+if (!$cmd_mode) {
+  if ($Log) {
+    $cmd_mode = 'log'
+  } elseif ($Regex) {
+    $cmd_mode = 'regex'
+  } elseif ($String) {
+    $cmd_mode = 'string'
+  } else {
+    $cmd_mode = 'log'
+  }
+}
+
+$base_command = ''
+
+# Git command to perform
+switch ($cmd_mode) {
+  'regex' {
+    $base_command = 'git log --oneline --branches --all -G {0}'
+  }
+  'string' {
+    $base_command = 'git log --oneline --branches --all -S {0}'
+  }
+  Default {
+    $base_command = 'git log --oneline --grep {0}'
+  }
+}
+
+$source_command = $base_command -f "'$Query'"
+$reload_command = $base_command -f '{q} || true'
+
+# Setup preview
+$fzf_preview = 'git show --color=always {1} '
+if (Get-Command -Name delta -All -ErrorAction SilentlyContinue) {
+  $fzf_preview="$fzf_preview | delta || true"
+} else {
+  $fzf_preview="$fzf_preview || true"
+}
+
+# It may be useful but prefer the initil pipe for now
+# --bind "start:reload:$source_command"
+
+# Call fzf
+$commits = [System.Collections.Generic.List[string]]::new()
+
+eval "$source_command" |
+  fzf `
+  --height 80% --min-height 20 --border `
+  --info=inline `
+  --bind 'ctrl-/:change-preview-window(down|hidden|)' `
+  --bind 'alt-up:preview-page-up,alt-down:preview-page-down' `
+  --bind 'ctrl-s:toggle-sort' `
+  --bind 'alt-f:first' `
+  --bind 'alt-l:last' `
+  --bind 'alt-c:clear-query' `
+  --prompt 'GitSearch> ' `
+  --header 'ctrl-r: interactive search | ctrl-f: Fzf filtering of results' `
+  --multi --ansi `
+  --layout=reverse `
+  --disabled `
+  --query "$Query" `
+  --bind "ctrl-r:unbind(ctrl-r)+change-prompt(GitSearch> )+disable-search+reload($reload_command)+rebind(change,ctrl-f)" `
+  --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(FzfFilter> )+enable-search+clear-query+rebind(ctrl-r)" `
+  --bind "change:reload:$reload_command" `
+  --preview "$fzf_preview" | ForEach-Object {
+    $line = $_ -split "\s+"
+    if ($line[0]) {
+      $commits.Add($line)
+    }
+  }
+
+# If no commits, exit
+if ($commits.Count -eq 0) {
+  exit
+}
+
+# Show selected commits
+if (!$Edit) {
+  git show @commits
+  exit
+}
+
+# Open in editor
+$tmpfile = New-TemporaryFile
+git show @commits > $tmpfile.FullName
+& "$editor" -c ":filetype detect" "$tmpfile"
+
