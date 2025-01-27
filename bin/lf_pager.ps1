daa8fabc5fbc4e1b02cb48491ce7e9e8b84ab50f
daniel <eduardo.daniel.s.m@gmail.com>
2025-01-26 23:24:53 -0500

    incomplete(lf_pager): lf_pager doesn't work when calling from windows
    The lf_pager script does not work when lf is started on windows even
    though it is calling the shell script version. It seems that there is an
    issue with bat using a pager as it causes the folling error:
    
    ```
    0 [main] less 3066 dtable::stdio_init: couldn't make stderr distinct from stdout, Win32 error 6
    ```
    
    This does not happen when lf is started in git bash/zsh which is weird
    as both shells should setup git bash as the running shell in windows.
    
    This pathc only addresses the detection under powershell and cmd but
    does not fix the pager or previews.

--- bin/lf_pager.ps1
+++ bin/lf_pager.ps1
@@ -1,5 +1,11 @@
 #!/usr/bin/env pwsh
 
+$user_conf_path = if ($env:user_conf_path) {
+  $env:user_conf_path
+} else {
+  "$HOME/.usr_conf"
+}
+
 # NOTE:
 # less uses the below variable to print unicode characters instead
 # of displaying the sequence. Needed to bat with paging=always flag.
@@ -7,4 +13,8 @@
 $env:LESSUTFCHARDEF = 'E000-F8FF:p,F0000-FFFFD:p,100000-10FFFD:p'
 
 & "$user_conf_path/utils/fzf-preview.ps1" $args | bat --style=plain --color=always --paging=always
+# & "$user_conf_path/utils/fzf-preview.ps1" $args | less -R
+
+# Write-Host -NoNewLine 'Press any key to continue...';
+# $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
