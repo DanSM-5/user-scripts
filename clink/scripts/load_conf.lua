612c53b48dc7ca3eed0349abf36496506859ca37
daniel <eduardo.daniel.s.m@gmail.com>
2025-01-26 19:29:13 -0500

    feat(clink): Add environment variables

--- clink/scripts/load_conf.lua
+++ clink/scripts/load_conf.lua
@@ -20,12 +20,19 @@ local home = os.getenv('USERPROFILE')
 os.setenv('HOME', home)
 os.setenv('user_scripts_path', home .. '\\user-scripts')
 os.setenv('user_conf_path', home .. '\\.usr_conf')
+os.setenv('user_config_cache', home .. '\\.cache\\.user_config_cache')
 os.setenv('prj', home .. '\\prj')
 
 -- Add ~/.local/bin at the top
 os.setenv('PATH', home .. '\\.local\\bin' .. ';'.. home .. '\\bin' .. ';' .. os.getenv('PATH') )
 -- Add ~/user-scripts/bin at the bottom
 os.setenv('PATH', os.getenv('PATH') .. ';' .. home .. '\\user-scripts\\bin')
+os.setenv('EDITOR', 'nvim')
+os.setenv('PREFERRED_EDITOR', 'nvim')
+os.setenv('BAT_THEME', 'OneHalfDark')
+os.setenv('COLORTERM', 'truecolor')
+os.setenv('WIN_ROOT', 'C:')
+os.setenv('WIN_HOME', home)
 
 -- function lf ()
 --   local r = io.popen("pwsh -nolo -nopro -nonin -c '" .. home .. "\\user-scripts\\bin\\lf.ps1'")
