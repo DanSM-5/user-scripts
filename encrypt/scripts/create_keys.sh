e3cc393b92e6e9b1a8bf213dca873ad670998d26
daniel <eduardo.daniel.s.m@gmail.com>
2025-02-03 16:54:34 -0500

    feat(encrypt): Add extra commands for creating rsa pem for JWT

--- encrypt/scripts/create_keys.sh
+++ encrypt/scripts/create_keys.sh
@@ -15,10 +15,14 @@ cd "$target" || exit
 pwd
 
 # Private key
-ssh-keygen -t rsa -b 4096 -m PEM -f RS256.key -q -N "$passphrase"
+ssh-keygen -t rsa -b 4096 -m PEM -f RS256-private.pem -q -N "$passphrase"
 # For powershell
 # ssh-keygen -b 2048 -t rsa -f C:/temp/sshkey -q -N '""'
+# Other valid commands
+# openssl genpkey -algorithm RSA -out RS256-private.pem -aes256
+# openssl genpkey -algorithm RSA -out RS256-private.pem -pkeyopt rsa_keygen_bits:2048
+# openssl genrsa -out RS256-private.pem 2048
 
 # Public key
-openssl rsa -in RS256.key -pubout -outform PEM -out RS256.pub
+openssl rsa -pubout -outform PEM -in RS256-private.pem -out RS256-public.pem
 
