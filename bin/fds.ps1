6d36c73482a91de15c6a3731e93fe2d569d7fee2
daniel <eduardo.daniel.s.m@gmail.com>
2025-02-20 21:55:35 -0500

    refactor: Change exclude file from arguments to `ignore-file`

--- bin/fds.ps1
+++ bin/fds.ps1
@@ -3,13 +3,13 @@
 $user_scripts_path = if ($env:user_scripts_path) { $env:user_scripts_path } else { "$HOME/user-scripts" }
 
 $fd_exclude_file = "$user_scripts_path/fd/fd_exclude"
-$fd_show_file = "$user_scripts_path/fd/fd_show"
+$fd_options_file = "$user_scripts_path/fd/fd_options"
 
-$fd_exclude = Get-Content $fd_exclude_file
-$fd_show = Get-Content $fd_show_file
+# $fd_exclude = Get-Content $fd_exclude_file
+$fd_options = Get-Content $fd_options_file
 
 # Write-Output "Arguments read from fd_exclude_file: $fd_exclude"
-# Write-Output "Arguments read from fd_show_file: $fd_show"
+# Write-Output "Arguments read from fd_options_file: $fd_options"
 
-& fd $fd_show $fd_exclude --color=always @args
+& fd $fd_options --ignore-file $fd_exclude_file @args
 
