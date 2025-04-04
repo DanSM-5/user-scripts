aee85ee9da0b0c2433f13a212c8358784bbecacb
daniel <eduardo.daniel.s.m@gmail.com>
2025-04-04 12:55:37 -0400

    fix(docker): Update docker install script

--- mint/docker-install.sh
+++ mint/docker-install.sh
@@ -13,8 +13,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$UBUNTU_CODENAME")" stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" |
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
