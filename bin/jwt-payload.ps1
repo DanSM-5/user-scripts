b36cd74d2ec165cf6b2526932f7d1a9f19510d23
daniel <eduardo.daniel.s.m@gmail.com>
2025-04-16 11:56:51 -0400

    feat(jwt-payload.ps1): Support stdin

--- bin/jwt-payload.ps1
+++ bin/jwt-payload.ps1
@@ -1,11 +1,15 @@
 #!/usr/bin/env pwsh
 
 Param(
+  [Parameter(ValueFromPipeline = $true)]
   [String] $RawJWT
 )
 
 # Get token from jwt
 [string] $token = ($RawJWT -Split '\.')[1]
+if (!$token) {
+  exit 1
+}
 # Calculate padding amount
 $pad = [System.Math]::Truncate(($token.Length + 3) / 4) * 4
 # Make it valid for dotnet
