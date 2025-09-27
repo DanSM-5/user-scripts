7f64ea8b6a17c69308dd4a414318aea6c02f8ef5
daniel <eduardo.daniel.s.m@gmail.com>
2025-09-27 14:55:17 -0400

    refactor(With-UTF8): Use codepage to change console encoding

--- powershell/With-UTF8.ps1
+++ powershell/With-UTF8.ps1
@@ -14,18 +14,25 @@ Param (
 # Script block info
 # Ref: https://stackoverflow.com/questions/11844390/how-do-i-pass-a-scriptblock-as-one-of-the-parameters-in-start-job
 
+$codepage = 0
+
 try {
   # Ref: https://stackoverflow.com/questions/49476326/displaying-unicode-in-powershell
   # Save the current settings and temporarily switch to UTF-8.
-  $oldOutputEncoding = $OutputEncoding;
-  $oldConsoleEncoding = [Console]::OutputEncoding
-  $OutputEncoding = [Console]::OutputEncoding = New-Object System.Text.Utf8Encoding
+  # $oldOutputEncoding = $OutputEncoding;
+  # $oldConsoleEncoding = [Console]::OutputEncoding
+  # $OutputEncoding = [Console]::OutputEncoding = New-Object System.Text.Utf8Encoding
+
+  # NOTE: Changing to codepage approach to prevent issues from cli programs that
+  # change codepage but fail to restore it properly
+  $codepage = ($(chcp) -split ' ')[3]
 
   # Execute block with utf-8 encoding
   return & $block
 } finally {
   # Restore the previous settings.
-  $OutputEncoding = $oldOutputEncoding;
-  [Console]::OutputEncoding = $oldConsoleEncoding
+  # $OutputEncoding = $oldOutputEncoding;
+  # [Console]::OutputEncoding = $oldConsoleEncoding
+  chcp $codepage *> $null
 }
 
