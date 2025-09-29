6a626e355bd5dcbfd4a6a877ed4150275e26c5a1
daniel <eduardo.daniel.s.m@gmail.com>
2025-09-28 22:49:21 -0400

    feat: allow to lock screen when turning monitor off

--- bin/monitor-off.ps1
+++ bin/monitor-off.ps1
@@ -1,5 +1,10 @@
 #!/usr/bin/env pwsh
 
+[CmdletBinding()]
+Param(
+  [Switch] $Lock = $false
+)
+
 # Ref: https://superuser.com/questions/1397941/how-to-turn-off-screen-with-powershell
 if ($env:IS_WINDOWS -eq 'true' -or $IsWindows) {
   # NOTE: PC is also locked
@@ -8,6 +13,20 @@ if ($env:IS_WINDOWS -eq 'true' -or $IsWindows) {
 
   # Other command
   # (Add-Type -MemberDefinition "[DllImport(""user32.dll"")]`npublic static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);" -Name "Win32SendMessage" -Namespace Win32Functions -PassThru)::SendMessage(0xffff, 0x0112, 0xF170, 2)
+
+
+  if ($Lock) {
+    $LockWorkStation = Add-Type -Name "Win32LockWorkStation" -PassThru -MemberDefinition @"
+[DllImport("user32.dll")]
+public static extern int LockWorkStation();
+"@
+  # Write-Information -MessageData "Locking workstation" -InformationAction Continue
+  # if (0 -eq $LockWorkStation::LockWorkStation()) {
+  #     throw 'Failed to lock workstation'
+  # }   
+
+    $LockWorkStation::LockWorkStation()
+  }
 } else {
   Write-Output 'TBI'
 }
