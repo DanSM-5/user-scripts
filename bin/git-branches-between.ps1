28522b070a5f0564f7a8faa516bff70212a3de6b
daniel <eduardo.daniel.s.m@gmail.com>
2026-02-13 20:56:46 -0500

    feat(git): add scripts to search branch names between two refs

---
+++ bin/git-branches-between.ps1
@@ -0,0 +1,76 @@
+#!/usr/bin/env pwsh
+
+<#
+.SYNOPSIS
+  Git search branches between commits
+
+.DESCRIPTION
+  List all branches (in alphabetical order) that exists between
+  two refs.
+  With no arguments it compares between master..HEAD
+  With a single argument it compares between master..<newest-ref>
+
+.PARAMETER FirstRef
+  Value of the oldest reference (default: HEAD)
+
+.PARAMETER SecondRef
+  Value of the newest reference (default: master)
+
+.PARAMETER Help
+  Show help message
+
+.INPUTS
+  No input from pipeline
+
+.OUTPUTS
+  List of branch names
+
+.EXAMPLE
+  git-branches-between -h
+
+.EXAMPLE
+  git-branches-between -Help
+
+.EXAMPLE
+  git-branches-between
+
+.EXAMPLE
+  git-branches-between HEAD
+
+.EXAMPLE
+  git-branches-between HEAD master
+
+.NOTES
+  List of branch names are sorted alphabetical and not  chronologicaly
+#>
+
+[CmdletBinding()]
+Param(
+  # Value of FirstRef
+  [string[]] $FirstRef = $null,
+  # Value of SecondRef
+  [string[]] $SecondRef = $null
+  # Show help
+  [Switch] $Help = $false,
+)
+
+$newestRef = ''
+$oldestRef =  ''
+
+if ($FirstRef -eq $null -and $SecondRef -eq $null) {
+  $newestRef = 'HEAD'
+  $oldestRef = 'master'
+} elseif ($FirstRef) {
+  $newestRef = 'HEAD'
+  $oldestRef = $FirstRef
+} else {
+  $newestRef = $FirstRef
+  $oldestRef = $SecondRef
+}
+
+[string[]]$commits = git rev-list "$oldest..$newest"
+
+$commits | ForEach-Object {
+  $commit = $_
+  (git branch --contains "$commit") -replace '^[* ] ', ''
+} | Sort-Object -Unique
