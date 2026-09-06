b913b30c6f53ac675c44ca34ed5680ddb66a50fe
daniel <eduardo.daniel.s.m@gmail.com>
2026-09-05 22:50:58 -0400

    feat: add yazi config

---
+++ yazi/plugins/confirm-quit.yazi/main.lua
@@ -0,0 +1,19 @@
+local count = ya.sync(function() return #cx.tabs end)
+
+local function entry()
+-- Confirm quit when multiple tabs are open
+	if count() < 2 then
+		return ya.emit("quit", {})
+	end
+
+	local yes = ya.confirm({
+		pos = { "center", w = 62, h = 10 },
+		title = "Quit?",
+		body = ui.Text("There are multiple tabs open. Are you sure you want to quit?"):wrap(ui.Wrap.YES),
+	})
+	if yes then
+		ya.emit("quit", {})
+	end
+end
+
+return { entry = entry }
